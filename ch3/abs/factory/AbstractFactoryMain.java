c965119f31a8766c01b5b83358b7adb8c1e4dd18
Kevin <108639920+kevinxu98@users.noreply.github.com>
2025-06-06 15:23:55 -0700

    finished abstract factory/factory design patterns

--- ch3/abs/factory/AbstractFactoryMain.java
+++ ch3/abs/factory/AbstractFactoryMain.java
@@ -5,5 +5,11 @@ public class AbstractFactoryMain {
     public static void main(String[] args){
         System.out.println("Factory Pattner: ");
         AbstractFactory carFactory = FactoryProvider.getFactory(FactoryNames.CAR);
+        AbstractFactory truckFactory = FactoryProvider.getFactory(FactoryNames.TRUCK);
+        Vehicle fastCar = carFactory.createVehicle(CarNames.FAST);
+        Vehicle heavyTruck = truckFactory.createVehicle(TruckNames.HEAVY);
+        fastCar.move();
+        heavyTruck.move();
+        
     }
 }
